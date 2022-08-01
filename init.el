76c529db5cf5589cada8552920eb49235e3ad5db
pmorawie <piotr.morawiec@nokia.com>
2022-08-01 13:22:35 +0200

    Adding yaml-mode and python-mode

--- init.el
+++ init.el
@@ -436,6 +436,29 @@
    (verilog-indent-level-directive 0)
    (verilog-indent-level-module 2))
 
+(use-package python-mode
+  :ensure t
+  :hook
+  (python-mode . lsp-deferred)
+  (python-mode . (lambda ()
+                   (setq indent-tabs-mode nil)
+                   (setq tab-width 4)
+                   (setq python-indent-offset 4)))
+  :custom
+  (python-shell-interpreter "python3"))
+
+(use-package yaml-mode
+  :ensure t)
+
+(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+
+;; Unlike python-mode, this mode follows the Emacs convention of not
+;; binding the ENTER key to `newline-and-indent'.  To get this
+;; behavior, add the key definition to `yaml-mode-hook':
+(add-hook 'yaml-mode-hook
+          '(lambda ()
+             (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+
 ;; --------------------------------------------------------------------------------------------
 ;; KEY BINDINGS
 ;; --------------------------------------------------------------------------------------------
